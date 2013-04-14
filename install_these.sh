238b88ddf8593ab5b2b2b78f6063d0ffba42feea
senturio <senturioman@gmail.com>
2013-04-14 19:58:02 +0100

    Initial commit

---
+++ install_these.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd -P`
+popd > /dev/null
+
+reqs_changed() {
+  echo Requirements have changed
+  echo New MD5sum:
+  cat requirements/base.txt requirements/test.txt | md5sum
+  echo
+  echo
+  pip install -r requirements/test.txt --index-url=http://simple.crate.io/
+  exit $?
+}
+
+(cat requirements/base.txt requirements/test.txt | md5sum --quiet -c $SCRIPTPATH/requirements.md5sum) || reqs_changed
+echo Requirements still OK
+
+HERED=$SCRIPTPATH/*-*
+LIST=""
+
+for fn in $HERED; do
+  LIST="$LIST file://$fn"
+done
+
+pip install --no-deps $LIST
